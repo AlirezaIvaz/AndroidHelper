4f2efec44e2d035ea4a660fd32baaa20fc1e38c7
Alireza Ivaz <info@alirezaivaz.ir>
2020-01-04 16:54:50 +0330

    Add MyView class

--- app/src/main/java/ir/android/example/MainActivity.java
+++ app/src/main/java/ir/android/example/MainActivity.java
@@ -3,12 +3,16 @@ package ir.android.example;
 import androidx.appcompat.app.AppCompatActivity;
 
 import android.os.Bundle;
+import android.view.View;
+
+import ir.android.helper.MyView;
 
 public class MainActivity extends AppCompatActivity {
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
-        setContentView(R.layout.activity_main);
+        View v = new MyView(this);
+        setContentView(v);
     }
 }
