d02e36b7b982f07c993e78dba663347ab853010a
Alireza Ivaz <info@alirezaivaz.ir>
2020-01-04 14:12:30 +0330

    Rename ToastMaker class

--- library/src/main/java/ir/android/helper/toast.java
+++ library/src/main/java/ir/android/helper/toast.java
@@ -3,7 +3,7 @@ package ir.android.helper;
 import android.content.Context;
 import android.widget.Toast;
 
-public class ToastMaker {
+public class toast {
     public static void ToastMaker (Context c, String m){
         Toast.makeText(c, m, Toast.LENGTH_SHORT).show();
     }
