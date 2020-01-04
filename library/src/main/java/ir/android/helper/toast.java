7642b91b433e708bf4e99a1e00d5ecb7dcd0837c
Alireza Ivaz <info@alirezaivaz.ir>
2020-01-04 18:16:49 +0330

    Add CustomToast

--- library/src/main/java/ir/android/helper/toast.java
+++ library/src/main/java/ir/android/helper/toast.java
@@ -1,13 +1,21 @@
 package ir.android.helper;
 
 import android.content.Context;
+import android.graphics.Typeface;
+import android.text.Spannable;
+import android.text.SpannableString;
 import android.widget.Toast;
 
 public class toast {
-    public static void ShortToast (Context c, String m){
-        Toast.makeText(c, m, Toast.LENGTH_SHORT).show();
+    public static void ShortToast (Context context, String text){
+        Toast.makeText(context, text, Toast.LENGTH_SHORT).show();
     }
-    public static void LongToast (Context c, String m) {
-        Toast.makeText(c, m, Toast.LENGTH_LONG).show();
+    public static void LongToast (Context c, String text) {
+        Toast.makeText(c, text, Toast.LENGTH_LONG).show();
+    }
+    public static void CustomToast (Context context, String text, Typeface typeface){
+        SpannableString efr = new SpannableString(text);
+        efr.setSpan(new FontProvider(typeface), 0, efr.length(), Spannable.SPAN_EXCLUSIVE_EXCLUSIVE);
+        Toast.makeText(context, efr, Toast.LENGTH_SHORT).show();
     }
 }
