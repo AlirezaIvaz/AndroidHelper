7670c0b71b6110d1e699666a8687b39612217063
Alireza Ivaz <info@alirezaivaz.ir>
2020-01-04 18:13:57 +0330

    Change layout

--- app/src/main/java/ir/android/example/MainActivity.java
+++ app/src/main/java/ir/android/example/MainActivity.java
@@ -1,18 +1,14 @@
 package ir.android.example;
 
 import androidx.appcompat.app.AppCompatActivity;
-
 import android.os.Bundle;
 import android.view.View;
-
-import ir.android.helper.MyView;
 
 public class MainActivity extends AppCompatActivity {
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
-        View v = new MyView(this);
-        setContentView(v);
+        setContentView(R.layout.activity_main);
     }
 }
